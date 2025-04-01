1a3b1e34da54e13356b79a23a38d51e853a8430f
UmeshJadhav2911 <usjadhav2911@gmail.com>
2025-04-01 18:03:18 +0530

    Self registration Email done

--- controllers/SellerProfile/createByAdmin.js
+++ controllers/SellerProfile/createByAdmin.js
@@ -4,6 +4,11 @@ import SellerProfileValidator from "../../validators/Seller/SellerProfile.js";
 import Seller from "../../models/Seller.js";
 import saltFunction from "../../validators/saltFunction.js";
 
+// Add Umesh 
+import nodemailer from "nodemailer";
+import SMTPEmailSetting from "../../models/SMTPEmailSetting.js";
+import SellerRegistrationEmailTemplate from "../../models/EmailTeamplates/SellerRegistrationEmailTemplate.js";
+
 function generateUserId() {
   const prefix = "SELID";
   const randomSuffix = Math.floor(Math.random() * 1000000);
@@ -20,6 +25,62 @@ function generateRandomPassword(length = 10) {
   }
   return password;
 }
+
+async function sendSellerRegistrationEmail(companyName, companyEmail, userCredentials) {
+  try {
+    const smtpSettings = await SMTPEmailSetting.findOne();
+    if (!smtpSettings) throw new Error("SMTP settings not found");
+
+    const emailTemplate = await SellerRegistrationEmailTemplate.findOne();
+    if (!emailTemplate) throw new Error("Email template not found");
+console.log(emailTemplate);
+
+    const transporter = nodemailer.createTransport({
+      host: smtpSettings.mailHost,
+      port: smtpSettings.mailPort,
+      secure: false,
+      auth: {
+        user: smtpSettings.mailUsername,
+        pass: smtpSettings.mailPassword,
+      },
+      tls: { rejectUnauthorized: false }
+    });
+
+    const credentialsHtml = `
+      <table border="1" cellpadding="5" cellspacing="0" style="border-collapse: collapse; width: 50%;">
+        <thead><tr><th>Role</th><th>UserID</th><th>Password</th></tr></thead>
+        <tbody>
+          <tr>
+            <td>Seller</td>
+            <td>${userCredentials.userId}</td>
+            <td>${userCredentials.password}</td>
+          </tr>
+        </tbody>
+      </table>
+    `;
+
+    const emailContent = emailTemplate.content
+      // .replace(/{CompanyName}/g, emailTemplate.mailFrom)
+      .replace(/{mailForm}/g, emailTemplate.mailFrom)
+      .replace(/{sellerCompanyName}/g, companyName)
+      .replace(/{Credentials}/g, credentialsHtml)
+      .replace(/{app_url}/g, smtpSettings.mailHost);
+
+    await transporter.sendMail({
+      from: `"${smtpSettings.mailFromName}" <${smtpSettings.mailFromAddress}>`,
+      to: companyEmail,
+      subject: emailTemplate.subject,
+      html: emailContent,
+    });
+
+    console.log("Seller registration email sent successfully");
+    return true;
+  } catch (error) {
+    console.error("Error sending registration email:", error);
+    return false;
+  }
+}
+
 
 async function createByAdmin(req, res) {
   try {
@@ -150,6 +211,8 @@ async function createByAdmin(req, res) {
     });
 
     await newSeller.save();
+
+    await sendSellerRegistrationEmail(companyName, emailId, { userId, password });
 
     return res.status(201).json({
       hasError: false,
