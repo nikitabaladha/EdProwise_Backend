055e71f80a39ff841a7538262ba637aa806e8f6c
nikitabaladha <nikitabaladha1996@gmail.com>
2025-05-15 09:29:21 +0530

    Shantanu new 2 file code merged

--- controllers/FeesModule/FeeReceipts/index.js
+++ controllers/FeesModule/FeeReceipts/index.js
@@ -3,9 +3,13 @@ import createSchoolFees from './SchoolFees/create.js';
 import getAdmissionForms from './BoardRegistrationFees/getstudentData.js';
 import createBoardRegistrationFeesPayment from './BoardRegistrationFees/create.js'
 
+import getAdmissionFormsBordExam from './BoardExamFee/getstudentData.js';
+import createBoardExamFeesPayment from './BoardExamFee/create.js'
 
 export {
     createSchoolFees,
     getAdmissionForms,
-    createBoardRegistrationFeesPayment
+    createBoardRegistrationFeesPayment,
+    getAdmissionFormsBordExam ,
+    createBoardExamFeesPayment
 };
